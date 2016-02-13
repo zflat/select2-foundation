ebd64687c581b16c6ca634684439624181a44e31
William Wedler <wwedler@riseup.net>
2016-02-13 16:33:46 -0500

    adding example of remote ajax call

--- test/styleguide/app/controllers/styleguide_controller.rb
+++ test/styleguide/app/controllers/styleguide_controller.rb
@@ -1,5 +1,9 @@
 class StyleguideController < ApplicationController #::ActionController::Base
   # layout false
   def show
+    respond_to do |format|
+      format.html
+      format.json { render json: nil }
+    end
   end
 end
